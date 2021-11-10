9dbc435bc7ba060e6a3c929a67d92f3e2b1b1734
Danil Rjazanov <89829876+Robot372@users.noreply.github.com>
2021-11-10 12:29:15 +0200

    Add files via upload

---
+++ rjazanov10.11.py
@@ -0,0 +1,14 @@
+from Modul import *
+while True:
+    print("Funktsioonid".center(50,"+"))
+    v=input("arithmetic- A,\nis_year_leap-Y")
+    v=input()
+    if v.upper()=="A":
+        arv1=float(input("Arv 1:"))
+        arv2=float(input("Arv 2:"))
+        sym=input("Tehe:")
+        rezult=arithmetic(arv1,arv2,sym)
+        print(rezult)
+    elif v.upper()=="Y":
+        rezult=is_year_leap(int(input("Sisesta aasta: ")))
+        print
